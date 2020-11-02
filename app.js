e078dc153e12d8da856aef5fc5b09b32e1bd8c45
NadegeMpore <68911672+NadegeMpore@users.noreply.github.com>
2020-11-02 10:52:03 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log(); //global object (can access it anywhere in any files)
+setTimeout(); // call a function after a delay
+clearTimeout();
+
+setInterval(); //
+clearInterval();
+window.console.log();
+
+var message = ""; // available to window
+global.setTimeout;
